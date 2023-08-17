28d1167fcceeed76353af0d92cc067356e223463
Ruhumuriza Justin <ruhumjustin@gmail.com>
2023-08-17 10:07:24 -0700

    Save

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_numbers - prints the numbers
+ *
+ * Return: as needed
+ */
+
+void print_numbers(void)
+{
+int i = 0;
+while (i < 10)
+{
+_putchar(i);
+i++;
+}
+_putchar('\n');
+}
