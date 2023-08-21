0cb73a994ca9c7504df0c0accc712c171f22e337
Ruhumuriza Justin <ruhumjustin@gmail.com>
2023-08-21 07:13:19 -0700

    pointers

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <main.h>
+
+/**
+ * main - update the code
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+int n = 402;
+int *p = &n;
+
+*p = 98
+
+_putchar("d\n", *p);
+
+return (0);
+}
