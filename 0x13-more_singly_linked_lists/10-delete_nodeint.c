97884d5bfd5562da32a3a715b68fa8a440699e2c
Ruhumuriza Justin <ruhumjustin@gmail.com>
2023-09-26 12:22:23 -0700

    coding is life

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -18,3 +18,40 @@ size_t _listint_len(const listint_t *h)
 
 	return (count);
 }
+
+/**
+ * delete_nodeint_at_index - deletes the node at index index of a listint_t.
+ * @head: linked lists
+ * @index: index to linked list
+ * Return: 1 success or -1 failed
+ */
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	unsigned int i = 0;
+	listint_t *tmp = *head, *node;
+
+	if (index > _listint_len(*head))
+		return (-1);
+
+	if (tmp &&  index == 0)
+	{
+		*head = tmp->next;
+		free(tmp);
+		return (1);
+	}
+
+	while (tmp && i < index)
+	{
+		node = tmp;
+		tmp = tmp->next;
+		i++;
+	}
+
+	if (!tmp)
+		return (-1);
+
+	node->next = tmp->next;
+	free(tmp);
+
+	return (1);
+}
